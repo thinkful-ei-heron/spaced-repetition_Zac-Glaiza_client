f9760e0eada30ed35aa42f5607560e4a107ebb69
Glaiza Wagner <glaizawagner@gmail.com>
2020-01-02 12:11:40 -0500

    api-endpoint updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://glaiza-zac-spaced-rep-server.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
